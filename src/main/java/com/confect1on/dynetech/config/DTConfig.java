d72b8347e2019df6ba69914efe5eb4dc3780a7ca
confect1on <me@confect1on.com>
2026-07-31 13:35:26 -0500

    fix duplicate fron merge lol

--- src/main/java/com/confect1on/dynetech/config/DTConfig.java
+++ src/main/java/com/confect1on/dynetech/config/DTConfig.java
@@ -41,11 +41,6 @@ public final class DTConfig {
             "minecraft:overworld -8 -64 -8 7 319 7"
     );
 
-    // 16x16 spawn zone in the overworld, bedrock to build limit. Sensible default AND doc example.
-    private static final List<String> DEFAULT_PROTECTED_REGIONS = List.of(
-            "minecraft:overworld -8 -64 -8 7 319 7"
-    );
-
     private static final List<String> DEFAULT_BLACKLIST = List.of(
             "minecraft:bedrock",
             "minecraft:barrier",
